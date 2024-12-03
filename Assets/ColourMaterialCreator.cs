6627316b62bd79b34dd9df71df882bfb60ba78a1
Aleks <s4204218@glos.ac.uk>
2024-12-03 17:52:06 +0000

    color blindness implementation

--- Assets/ColourMaterialCreator.cs
+++ Assets/ColourMaterialCreator.cs
@@ -6,23 +6,23 @@ using UnityEditor;
 [ExecuteInEditMode]
 public class ColourMaterialCreator : MonoBehaviour
 {
-    [SerializeField] private Color objectColor = Color.white;
+    public Color objectColour = Color.white;
 
     //when theres a change in the editor
     void OnValidate()
     {
         //apply new color to object
-        ApplyMaterial();
+        ApplyMaterial(objectColour);
     }
 
     //function that creates a new material and changes its color
-    void ApplyMaterial()
+    public void ApplyMaterial(Color colour)
     {
         //create a new material with a basic shader (Standard when not using URP)
         Material newMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
 
         //set the color of the material
-        newMaterial.color = objectColor;
+        newMaterial.color = colour;
 
         // Apply the material to the object's renderer
         Renderer renderer = GetComponent<Renderer>();
